dd42bc49abd72a2c677fadf0c38304d662e586c3
b1ngusbangus <156984703+b1ngusbangus@users.noreply.github.com>
2024-02-05 19:39:16 -0800

    Update AList.java

--- lec7_lists4/DIY/AList.java
+++ lec7_lists4/DIY/AList.java
@@ -5,31 +5,43 @@ package lec7_lists4.DIY;
  *  After writing your methods, you can run the AListTest file.
  */
 public class AList {
+    // whole entire thing will have a size and start off with an empty list
+    private int[] items;   // set to private so "no outsider can edit our code" -hug
+    private int size;
+    
     /** Creates an empty list. */
     public AList() {
+        items = new int[1000]
+        size = 0;
     }
 
     /** Inserts X into the back of the list. */
     public void addLast(int x) {
+        items[size] = x;   // size starts at 0 which is = 0th index in empty list --> add x there
+        size += 1;   // increase size by 1 and repeat
     }
 
     /** Returns the item from the back of the list. */
     public int getLast() {
-        return 0;        
+        return items[size - 1];   // length starts at 1, index starts at 0 --> size-1 to get last item since
+                                  // we're referring to indices here
     }
     /** Gets the ith item in the list (0 is the front). */
     public int get(int i) {
-        return 0;        
+        return item[i];        
     }
 
     /** Returns the number of items in the list. */
     public int size() {
-        return 0;        
+        return size;        
     }
 
     /** Deletes item from back of the list and
       * returns deleted item. */
     public int removeLast() {
-        return 0;
+        int deletedValue = items[size - 1];
+        items[size - 1] = null;
+        size -= 1;
+        return deletedValue;
     }
 } 
